971e60425af7473412e57e80bb0183d782a2ad9f
Spoo69420 <55297041+Spoo69420@users.noreply.github.com>
2020-03-05 15:40:51 +0000

    Update interpreter.lua

--- interpreter.lua
+++ interpreter.lua
@@ -1,5 +1,5 @@
 --[[
-pngview: Simple high-resolution PNG viewer 
+ctif-oc: OpenComputers viewer for CTIF image files
 Copyright (c) 2016, 2017, 2018 asie
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -22,7 +22,6 @@ SOFTWARE.
 ]]
 
 local args = {...}
-local ocpng = require("png")
 local component = require("component")
 local event = require("event")
 local gpu = component.gpu
@@ -31,21 +30,6 @@ local keyboard = require("keyboard")
 local text = require("text")
 local os = require("os")
 local pal = {}
-
--- 4x4 dithermask
---local oDither = {
---	0, 8, 2, 10,
---	12, 4, 14, 6,
---	3, 11, 1, 9,
---	15, 7, 13, 5
---}
--- 2x2 dithermask
-local oDither = {
-	0, 2,
-	3, 1
-}
-local oDSize = #oDither
-local oDWidth = math.floor(math.sqrt(#oDither))
 
 local q = {}
 for i=0,255 do
@@ -60,19 +44,20 @@ for i=0,255 do
   q[i + 1] = unicode.char(0x2800 | dat)
 end
 
-local function round(v)
-  return math.floor(v + 0.5)
+function error(str)
+  print("ERROR: " .. str)
+  os.exit()
 end
 
 function resetPalette(data)
  for i=0,255 do
   if (i < 16) then
---    if data == nil or data[3] == nil or data[3][i] == nil then
+    if data == nil or data[3] == nil or data[3][i] == nil then
       pal[i] = (i * 15) << 16 | (i * 15) << 8 | (i * 15)
---    else
---      pal[i] = data[3][i]
---      gpu.setPaletteColor(i, data[3][i])
---    end
+    else
+      pal[i] = data[3][i]
+      gpu.setPaletteColor(i, data[3][i])
+    end
   else
     local j = i - 16
     local b = math.floor((j % 5) * 255 / 4.0)
@@ -85,145 +70,92 @@ end
 
 resetPalette(nil)
 
-local function pngSafeGet(png, x, y, paletted)
-  if x >= png.w then x = png.w - 1 elseif x < 0 then x = 0 end
-  if y >= png.h then y = png.h - 1 elseif y < 0 then y = 0 end
-  return png:get(x, y, paletted)
-end
-
-local function maxAbs(r, g, b)
-  local d1 = r - g
-  local d2 = g - b
-  if d1 < 0 then d1 = -d1 end
-  if d2 < 0 then d2 = -d2 end
-  if d1 >= d2 then return d1 else return d2 end
-end
-
-local function getOCPalEntry(png, data, rgb)
-  -- TODO: more cleverness
-  local r = (rgb >> 16) & 0xFF
-  local g = (rgb >> 8) & 0xFF
-  local b = (rgb) & 0xFF
-  if maxAbs(r, g, b) <= 6 then
-    local i = round(g * 16.0 / 255.0)
-    if i <= 0 then return 16 elseif i >= 16 then return 255 else return i - 1 end
+function r8(file)
+  local byte = file:read(1)
+  if byte == nil then
+    return 0
   else
-    return 16 + (round(r * 5.0 / 255.0) * 40) + (round(g * 7.0 / 255.0) * 5) + round(b * 4.0 / 255.0)
+    return string.byte(byte) & 255
   end
 end
 
-local function colorDistSq(rgb1, rgb2)
-  if rgb1 == rgb2 then return 0 end
-
-  local r1 = (rgb1 >> 16) & 0xFF
-  local g1 = (rgb1 >> 8) & 0xFF
-  local b1 = (rgb1) & 0xFF
-  local r2 = (rgb2 >> 16) & 0xFF
-  local g2 = (rgb2 >> 8) & 0xFF
-  local b2 = (rgb2) & 0xFF
-  local rs = (r1 - r2) * (r1 - r2)
-  local gs = (g1 - g2) * (g1 - g2)
-  local bs = (b1 - b2) * (b1 - b2)
-  local rAvg = math.floor((r1 + r2) / 2)
-
-  return (((512 + rAvg) * rs) >> 8) + (4 * gs) + (((767 - rAvg) * bs) >> 8)
-end
-
--- inspired by the teachings of Bisqwit
--- http://bisqwit.iki.fi/story/howto/dither/jy/
-local function ditherDistance(rgb, rgb1, rgb2)
-  if rgb1 == rgb2 then return 0.5 end
-
-  local r = (rgb >> 16) & 0xFF
-  local g = (rgb >> 8) & 0xFF
-  local b = (rgb) & 0xFF
-  local r1 = (rgb1 >> 16) & 0xFF
-  local g1 = (rgb1 >> 8) & 0xFF
-  local b1 = (rgb1) & 0xFF
-  local r2 = (rgb2 >> 16) & 0xFF
-  local g2 = (rgb2 >> 8) & 0xFF
-  local b2 = (rgb2) & 0xFF
-
-  return (r * r1 - r * r2 - r1 * r2 + r2 * r2 +
-    g * g1 - g * g2 - g1 * g2 + g2 * g2 +
-    b * b1 - b * b2 - b1 * b2 + b2 * b2) /
-    ((r1 - r2) * (r1 - r2) +
-      (g1 - g2) * (g1 - g2) +
-      (b1 - b2) * (b1 - b2));
+function r16(file)
+  local x = r8(file)
+  return x | (r8(file) << 8)
 end
 
 function loadImage(filename)
-  local png = ocpng.loadPNG(filename)
-
-  local charH = math.ceil(png.h / 4.0)
-  local charW = math.ceil(png.w / 2.0)
   local data = {}
+  local file = io.open(filename, 'rb')
+  local hdr = {67,84,73,70}
 
-  data[1] = {}
-  data[2] = {charW, charH}
-  data[3] = {}
-
-  -- todo: populate with customColors [3][0]...
-
-  local j = 1
-  for y=0,charH-1 do
-    for x=0,charW-1 do
-      local pixelsRGB = {
-        pngSafeGet(png, x*2+1, y*4+3),
-        pngSafeGet(png, x*2, y*4+3),
-        pngSafeGet(png, x*2+1, y*4+2),
-        pngSafeGet(png, x*2, y*4+2),
-        pngSafeGet(png, x*2+1, y*4+1),
-        pngSafeGet(png, x*2, y*4+1),
-        pngSafeGet(png, x*2+1, y*4),
-        pngSafeGet(png, x*2, y*4)
-      }
-      local pixels = {}
-      for i=1,#pixelsRGB do pixels[i] = getOCPalEntry(png, data, pixelsRGB[i]) end
-      -- identify most common colors
-      local pixelCount = {}
-      local pixelColors = 0
-      for i=1,#pixels do
-        if pixelCount[pixels[i]] == nil then pixelColors = pixelColors + 1 end
-        pixelCount[pixels[i]] = (pixelCount[pixels[i]] or 0) + 1
-      end
-      if pixelColors == 1 then
-        data[1][j] = (0 << 16) | (0 << 8) | pixels[1]
-      else
-        local bg = -1
-        local bgc = -1
-        local fg = -1
-        local fgc = -1
-        for k,v in pairs(pixelCount) do
-          if v > bgc then
-            bg = k
-            bgc = v
-          end
-        end
-        for k,v in pairs(pixelCount) do
-          local contrast = colorDistSq(pal[bg], pal[k]) * v
-          if k ~= bg and contrast > fgc then
-            fg = k
-            fgc = contrast
-          end
-        end
-        local chr = 0
-        for i=1,#pixels do
-          local px = x * 2 + ((i - 1) & 1)
-          local py = y * 4 + ((i - 1) >> 1)
-          local dDist = ditherDistance(pixelsRGB[i], pal[bg], pal[fg])
-          local dDi = oDSize - round(dDist * oDSize)
-          local dThr = oDither[1 + ((py % oDWidth) * oDWidth) + (px % oDWidth)]
-          if dThr < dDi then
-            chr = chr | (1 << (i - 1))
-          end
-        end
-        data[1][j] = bg | (fg << 8) | (chr << 16)
-      end
-      j = j + 1
+  for i=1,4 do
+    if r8(file) ~= hdr[i] then
+      error("Invalid header!")
     end
   end
 
+  local hdrVersion = r8(file)
+  local platformVariant = r8(file)
+  local platformId = r16(file)
+
+  if hdrVersion > 1 then
+    error("Unknown header version: " .. hdrVersion)
+  end
+
+  if platformId ~= 1 or platformVariant ~= 0 then
+    error("Unsupported platform ID: " .. platformId .. ":" .. platformVariant)
+  end
+
+  data[1] = {}
+  data[2] = {}
+  data[3] = {}
+  data[2][1] = r8(file)
+  data[2][1] = (data[2][1] | (r8(file) << 8))
+  data[2][2] = r8(file)
+  data[2][2] = (data[2][2] | (r8(file) << 8))
+
+  local pw = r8(file)
+  local ph = r8(file)
+  if not (pw == 2 and ph == 4) then
+    error("Unsupported character width: " .. pw .. "x" .. ph)
+  end
+
+  data[2][3] = r8(file)
+  if (data[2][3] ~= 4 and data[2][3] ~= 8) or data[2][3] > gpu.getDepth() then
+    error("Unsupported bit depth: " .. data[2][3])
+  end
+
+  local ccEntrySize = r8(file)
+  local customColors = r16(file)
+  if customColors > 0 and ccEntrySize ~= 3 then
+    error("Unsupported palette entry size: " .. ccEntrySize)
+  end
+  if customColors > 16 then
+    error("Unsupported palette entry amount: " .. customColors)
+  end
+
+  for p=0,customColors-1 do
+    local w = r16(file)
+    data[3][p] = w | (r8(file) << 16)
+  end
+
+  local WIDTH = data[2][1]
+  local HEIGHT = data[2][2]
+
+  for y=0,HEIGHT-1 do
+    for x=0,WIDTH-1 do
+      local j = (y * WIDTH) + x + 1
+      local w = r16(file)
+      if data[2][3] > 4 then
+        data[1][j] = w | (r8(file) << 16)
+      else
+        data[1][j] = w
+      end
+    end
+  end
+
+  io.close(file)
   return data
 end
 
@@ -268,9 +200,15 @@ function drawImage(data, offx, offy)
     local str = ""
     for x=0,WIDTH-1 do
       ind = (y * WIDTH) + x + 1
-      bg = pal[data[1][ind] & 0xFF]
-      fg = pal[(data[1][ind] >> 8) & 0xFF]
-      cw = ((data[1][ind] >> 16) & 0xFF) + 1
+      if data[2][3] > 4 then
+        bg = pal[data[1][ind] & 0xFF]
+        fg = pal[(data[1][ind] >> 8) & 0xFF]
+        cw = ((data[1][ind] >> 16) & 0xFF) + 1
+      else
+        fg = pal[data[1][ind] & 0x0F]
+        bg = pal[(data[1][ind] >> 4) & 0x0F]
+        cw = ((data[1][ind] >> 8) & 0xFF) + 1
+      end
       noBG = (cw == 256)
       noFG = (cw == 1)
       if (noFG or (gBG == fg)) and (noBG or (gFG == bg)) then
